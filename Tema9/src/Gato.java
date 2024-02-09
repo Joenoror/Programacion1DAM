d3ca8ee5e1915f327b0d29956857fe9a631abc58
Joenoror <falanhx@gmail.com>
2024-02-09 11:19:49 +0100

    Interface Examples

---
+++ Tema9/src/Gato.java
@@ -0,0 +1,6 @@
+public class Gato implements Sonido{
+    @Override
+    public void voz() {
+        System.out.println("Miau");
+    }
+}
---
+++ Tema9/src/Perro.java
@@ -0,0 +1,6 @@
+public class Perro implements Sonido{
+    @Override
+    public void voz() {
+        System.out.println("Guau!");
+    }
+}
